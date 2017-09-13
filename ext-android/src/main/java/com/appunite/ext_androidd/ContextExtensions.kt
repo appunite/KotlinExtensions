47d95f82d3cbcd14116ec724ccf66b2e82e08cc8
Marcin Adamczewski <marcin.adamczewski@appunite.com>
2017-09-13 15:23:00 +0200

    Fixed shaded extensions

--- ext-android/src/main/java/com/appunite/ext_androidd/ContextExtensions.kt
+++ ext-android/src/main/java/com/appunite/ext_androidd/ContextExtensions.kt
@@ -3,7 +3,7 @@ package com.appunite.ext_androidd
 import android.content.Context
 import android.support.v4.content.ContextCompat
 
-fun Context.checkSelfPermission(permissions: String): Int = ContextCompat.checkSelfPermission(this, permissions)
+fun Context.checkPermission(permissions: String): Int = ContextCompat.checkSelfPermission(this, permissions)
 
 
 
